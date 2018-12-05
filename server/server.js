fc9df83993d2db8b55da7f46e2d5ec103aa11de9
carrie <carriewan7@gamail.com>
2018-12-05 11:10:51 +0800

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require(`body-parser`);
 var {mongoose} = require(`./db/mongoose`);
 var {Todo} = require(`./models/todo`);
 var {User} = require(`./models/user`);
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -32,12 +33,17 @@ app.get(`/todos`, (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      res.status(400).send();
+      res.status(404).send();
     }
-    res.send(todo);
-  }, (e) => {
+    res.send({todo});
+  }).catch((e) => {
     res.status(400).send();
   });
 });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,10 +3,13 @@ const request = require(`supertest`);
 
 const {app} = require(`./../server`);
 const {Todo} = require(`./../models/todo`);
+var {ObjectID} = require('mongodb');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -70,6 +73,33 @@ describe(`GET /todos`, () => {
     .expect((res) => {
       expect(res.body.todos.length).toBe(2);
     })
+    .end(done);
+  });
+});
+
+describe(`GET /todos/:id`, () => {
+  it(`should return todo doc`, (done) => {
+    request(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(todos[0].text);
+    })
+    .end(done);
+  });
+
+  it(`should return 404 if todo not found`, (done) => {
+    var otherId = new ObjectID();
+    request(app)
+    .get(`/tdods/${otherId.toHexString()}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it(`should return 404 for non-object ids`, (done) => {
+    request(app)
+    .get(`/tdods/${`AAAAA`}`)
+    .expect(404)
     .end(done);
   });
 });
